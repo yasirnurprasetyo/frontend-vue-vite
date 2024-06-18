0096d05f7bd2cc556c078ca03672243d80e02612
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2024-06-18 14:11:00 +0700

    add dashboard and proses login

---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <div class="card bourder-0 rounded shadow-sm">
+        <div class="card-header">MENU UTAMA</div>
+    </div>
+    <div class="card-body">
+        <div class="list-group">
+            <router-link :to="{name: 'dashboard'}" class="list-group-item list-group-item-action">DASHBOARD</router-link>
+            <a href="#" class="list-group-item list-group-item-action">Users</a>
+            <a href="#" class="list-group-item list-group-item-action">Logout</a>
+        </div>
+    </div>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Cookie from "js-cookie";
+const getToken = () => Cookie.get('token')
 
 const routes = [
     {
@@ -13,12 +15,28 @@ const routes = [
         path: '/login',
         name:'login',
         component: () => import('../views/auth/login.vue')
+    },{
+        path: '/admin/dashboard',
+        name: 'dashboard',
+        component: () => import('../views/admin/dashboard/index.vue'),
+        meta: { requiresAuth: true}
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const token = getToken()
+    if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+        next({name:'login'})
+    }else if((to.name === 'login' || to.name === 'register') && token){
+        next({ name: 'dashboard'})
+    }else {
+        next()
+    }
 })
 
 export default router
